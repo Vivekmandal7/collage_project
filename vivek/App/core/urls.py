ba688633b209cfe3959aa23b34db6de34690ffb4
Vivekmandal7 <118199128+Vivekmandal7@users.noreply.github.com>
2024-02-24 16:30:07 +0530

    ok

--- vivek/App/core/urls.py
+++ vivek/App/core/urls.py
@@ -1,12 +1,9 @@
-from django.contrib import admin
+# urls.py
 from django.urls import path
 from . import views
 
-# urlpatterns = [
-#     path('',views.index,name="hi"),
-# ]
-
-
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home'),             # URL pattern for home.html (root URL)
+    path('home.html', views.home, name='home'),     # Additional URL pattern for home.html
+    path('hone.html', views.hone, name='hone'),     # URL pattern for hone.html
 ]
